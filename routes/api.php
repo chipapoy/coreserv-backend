86818fecb037a16b130f98cbbebfe1456d034c17
unknown <MikSingson@SKYLT2498.skycable.com>
2022-11-25 06:07:37 +0800

    edit cors.php

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::get('/user', function (Request $request) {
     return $request->user();
 });
-
 
 
 Route::get('/users/{id}', function ($id) {
